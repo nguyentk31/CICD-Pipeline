b210290d38a3496a4ef668c11a878c73085e14ac
nguyentk31 <tkn3101@gmail.com>
2024-05-01 17:07:26 +0700

    test push workflow 3/7

--- test_infra/modules/ecr/main.tf
+++ test_infra/modules/ecr/main.tf
@@ -25,35 +25,8 @@ data "aws_iam_policy_document" "ga-assumerole" {
   }
 }
 
-# ECR Push Assume Role Policy
-data "aws_iam_policy_document" "ecr-push" {
-  statement {
-    actions = [
-      "ecr:CompleteLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:InitiateLayerUpload",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage"
-    ]
-    effect = "Allow"
-    resources = [
-      aws_ecr_repository.image.arn,
-      aws_ecr_repository.chart.arn
-    ]
-  }
-
-  statement {
-    actions   = ["ecr:GetAuthorizationToken"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-}
-
 resource "aws_iam_role" "github-actions" {
   name               = "GithubActionsRole"
   assume_role_policy = data.aws_iam_policy_document.ga-assumerole.json
-  inline_policy {
-    name   = "ecr-push"
-    policy = data.aws_iam_policy_document.ecr-push.json
-  }
+  managed_policy_arns = [ "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser" ]
 }
